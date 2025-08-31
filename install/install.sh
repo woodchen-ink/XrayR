665f8c529e05c7979446e71cea2bf9324f958b7e
wood chen <wood@czl.net>
2025-08-31 08:30:15 +0800

    fix(install): 更新XrayR安装脚本的下载链接
    将XrayR安装脚本的下载链接从release分支更新为master分支，确保获取最新版本的安装脚本

--- install/install.sh
+++ install/install.sh
@@ -182,7 +182,7 @@ install_XrayR() {
     if [[ ! -f /etc/XrayR/rulelist ]]; then
         cp rulelist /etc/XrayR/
     fi
-    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/woodchen-ink/XrayR-release/master/XrayR.sh
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/woodchen-ink/XrayR/master/install/XrayR.sh
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # 小写兼容
     chmod +x /usr/bin/xrayr
